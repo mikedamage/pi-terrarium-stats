cd499d6c985c0944cf5ee3a1c01346d09567b22f
Mike Green <mike.is.green@gmail.com>
2021-10-05 17:52:54 -0500

    read sensors using promise wrapper function

--- bin/read-dht.js
+++ bin/read-dht.js
@@ -1,21 +1,9 @@
-const sensor = require('node-dht-sensor')
-const argv = require('yargs').argv
+const sensor = require("node-dht-sensor");
+const argv = require("yargs").argv;
+const { readDHT } = require("../lib/read-dht");
 
-const gpioPin = 4
+const [pin] = argv._;
 
-function toFahrenheit(temp) {
-  return (temp * (9 / 5)) + 32
-}
-
-function readSensor() {
-  return new Promise((resolve, reject) => {
-    sensor.read(11, gpioPin, (err, temp, humidity) => {
-      if (err) return reject(err)
-      resolve({ temp, humidity })
-    })
-  })
-}
-
-readSensor().then(({ temp, humidity }) => {
-  console.log(`Temp: ${toFahrenheit(temp)}F, Humidity: ${humidity}%`)
-})
+readDHT(pin).then(({ temp, humidity }) => {
+  console.log(`Temp: ${temp}F, Humidity: ${humidity}%`);
+});
