76fb20935d89c4dd54bcd33bfda13f768201a8bb
Mike Green <mike.is.green@gmail.com>
2021-10-14 01:40:53 -0500

    modularize metrics

--- bin/terrarium-stats.js
+++ bin/terrarium-stats.js
@@ -1,44 +1,23 @@
-const { readDHT } = require('../lib/read-dht')
-const { Registry, Gauge } = require('prom-client')
-const { readFile } = require('fs/promises')
+const { Registry } = require('prom-client')
 const express = require('express')
 const app = express()
+
+// Metrics
+const temperatureGauge = require('../lib/metrics/temperature')
+const humidityGauge = require('../lib/metrics/humidity')
+const cpuTemp = require('../lib/metrics/cpu-temp')
+const gpuTemp = require('../lib/metrics/gpu-temp')
+
 const port = process.env.PORT || 8000
 const sensorPin = process.env.SENSOR_GPIO || 4
-
 const registry = new Registry()
 
-const temperature = new Gauge({
-  name: 'terrarium_temperature',
-  help: 'Terrarium temperature in Celsius',
-  async collect() {
-    const { temp } = await readDHT(sensorPin)
-    this.set(temp)
-  },
-  registers: [registry],
-})
+registry.registerMetric(temperatureGauge(sensorPin))
+registry.registerMetric(humidityGauge(sensorPin))
+registry.registerMetric(cpuTemp())
+registry.registerMetric(gpuTemp())
 
-const humidity = new Gauge({
-  name: 'terrarium_humidity',
-  help: 'Terrarium humidity in percent',
-  async collect() {
-    const { humidity } = await readDHT(sensorPin)
-    this.set(humidity)
-  },
-  registers: [registry],
-})
-
-const piCpuTemp = new Gauge({
-  name: 'pi_cpu_temp',
-  help: 'Terrarium Pi CPU temperature in Celsius',
-  async collect() {
-    const rawTemp = await readFile('/sys/class/thermal/thermal_zone0/temp')
-    this.set(parseInt(rawTemp, 10) / 1000)
-  },
-  registers: [registry],
-})
-
-app.get('/', async (req, res) => {
+app.get('/', async (_req, res) => {
   const metrics = await registry.metrics()
   res.send(metrics)
 })
@@ -46,4 +25,3 @@ app.get('/', async (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
-
